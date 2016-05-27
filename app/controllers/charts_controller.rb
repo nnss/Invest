88a1ae9bf9d4ff919b61d90a9f6e0045674854e6
matias <matiaspalomec@gmail.com>
2016-05-27 18:53:37 -0300

    improve graphics, now are grouped by invest name

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -11,14 +11,35 @@ class ChartsController < ApplicationController
     end
   end
   def week_glist
-    render json: Invest.where("user_id = ? and date(created_at) >= ?",@current_user,Date.today-1.week).pluck("date(created_at) as created_at, amount")
+    res = {}
+    res2 = []
+    Invest.where("user_id = ? and date(created_at) >= ?",@current_user,Date.today-1.week).group("name,created_at").pluck("date(created_at) as created_at, amount, name").map do |n,v,a|
+        res[a] = Array.new() if res[a].nil?
+        res[a] << [n,v]
+   end
+   res.each_pair{|k,v| res2<<{name: k, data: v}}
+   render json: res2
   end
 
   def year_glist
-    render json: Invest.where("user_id = ? and date(created_at) >= ?",@current_user,Date.today-1.year).pluck("date(created_at) as created_at, amount")
+    res = {}
+    res2 = []
+    Invest.where("user_id = ? and date(created_at) >= ?",@current_user,Date.today-1.year).group("name,created_at").pluck("date(created_at) as created_at, amount, name").map do |n,v,a|
+        res[a] = Array.new() if res[a].nil?
+        res[a] << [n,v]
+   end
+   res.each_pair{|k,v| res2<<{name: k, data: v}}
+   render json: res2
   end
 
   def month_glist
-    render json: Invest.where("user_id = ? and date(created_at) >= ?",@current_user,Date.today-1.month).pluck("date(created_at) as created_at, amount")
+    res = {}
+    res2 = []
+    Invest.where("user_id = ? and date(created_at) >= ?",@current_user,Date.today-1.month).group("name,created_at").pluck("date(created_at) as created_at, amount, name").map do |n,v,a|
+        res[a] = Array.new() if res[a].nil?
+        res[a] << [n,v]
+   end
+   res.each_pair{|k,v| res2<<{name: k, data: v}}
+   render json: res2
   end
 end
